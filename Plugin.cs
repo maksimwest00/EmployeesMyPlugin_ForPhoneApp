a994b31e11df9e222df773ab0250b22c0b3be0d4
Maks <Maks@DESKTOP-J4F0VBN>
2023-07-25 19:13:20 +0300

    Добавьте файлы проекта.

---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeesMyPlugin
+{
+    [Author(Name = "Maksim Sukharev")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("{My} Loading employees");
+
+            List<EmployeesDTO> employeesList = JsonConvert.DeserializeObject<List<EmployeesDTO>>(EmployeesMyPlugin.Properties.MyResources.EmployeesJson);
+
+            logger.Info($"{{My}} Loaded {employeesList.Count()} employees");
+            return employeesList.Cast<DataTransferObject>();
+        }
+    }
+}
